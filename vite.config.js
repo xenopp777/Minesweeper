91436ca6d7b4d6e6b1fcc1af90383e651565ce88
xenopp777 <zoied@outlook.com>
2026-06-09 11:15:05 -0700

    added config files for production

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+﻿import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import bundleCss from "vite-plugin-bundle-css";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  base: "./",
+  build: {
+    cssCodeSplit: true,
+  },
+  plugins: [
+    bundleCss({
+      name: "bundle.css",
+      fileName: "bundle.css",
+      include: ["./src/css/styles.css"],
+      mode: "import",
+    }),
+  ],
+  server: {
+    open: "/index.html",
+  },
+};
